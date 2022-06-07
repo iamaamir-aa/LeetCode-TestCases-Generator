4bc5941fe504537de85f6baf046e00295d661d27
Aamir Ansari <75007828+iamaamir-aa@users.noreply.github.com>
2022-06-07 11:42:25 +0530

    Remove Unnecessary Comments

--- TestCasesGenerator.java
+++ TestCasesGenerator.java
@@ -1,6 +1,5 @@
-// Online Java Compiler
-// Use this editor to write, compile and run your Java code online
 import java.util.*;
+
 public class Main {
     public static void main(String[] args) {
         Scanner s=new Scanner(System.in);
